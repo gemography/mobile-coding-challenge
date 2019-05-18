9ba63b9424b894a58a7f2eddafd2981e9ca19edd
saber el harti <saberelharti@gmail.com>
2019-05-18 12:23:45 +0000

    Add bottom navigation bar

--- app/src/main/java/io/psisoft/codechallenge/MainActivity.java
+++ app/src/main/java/io/psisoft/codechallenge/MainActivity.java
@@ -1,13 +1,50 @@
 package io.psisoft.codechallenge;
 
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.container)
+    FrameLayout container;
+    @BindView(R.id.navigation_view)
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.container, new Trending(), "feed").commit();
+        bottomNavigationView.setSelectedItemId(R.id.feed);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (item -> {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.feed:
+                            selectedFragment = new Trending();
+                            break;
+                        case R.id.setting:
+                            selectedFragment = new Settings();
+                            break;
+                    }
+
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.container, selectedFragment);
+                    transaction.commit();
+                    return true;
+                });
     }
+
 }
