4f8205e1c7449f60644e231f5477eebe45fe5729
mtkahn <contact@muhammadtalha.com>
2019-08-03 07:51:46 +0500

    initial commit adding gitignore and Project Files

---
+++ mobile-coding-challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mobile-coding-challenge
+//
+//  Created by Talha on 03/08/2019.
+//  Copyright © 2019 Talha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
