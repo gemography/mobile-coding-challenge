642a5918bda2a0fb0a41bc7e2ba97dd02b21b406
Mobile <mobile@MacBook-Pro-de-Nabil.local>
2020-09-19 16:04:07 +0100

    Creating empty project

---
+++ mobile-coding-challenge/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  mobile-coding-challenge
+//
+//  Created by Mobile on 9/19/20.
+//  Copyright © 2020 Zakariae. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
